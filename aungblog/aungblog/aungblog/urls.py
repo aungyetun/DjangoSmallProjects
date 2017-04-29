1850d72594625a8632400e5e041a7a30256abfa9
aungyetun <aungthantzinwin@gmail.com>
2017-04-29 16:58:50 -0700

    Image Display

--- aungblog/aungblog/aungblog/urls.py
+++ aungblog/aungblog/aungblog/urls.py
@@ -22,4 +22,4 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.home)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
